c9a7fb070805938ff08c6bed0f0a6c0a4816c13c
kongjunyihao <xutianxiang47@gmail.com>
2022-10-25 12:00:02 -0700

    Style

--- src/LoginForm.js
+++ src/LoginForm.js
@@ -1,4 +1,10 @@
 import React, { useState } from "react";
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 import "./CSS/loginformStyle.css";
 
 function LoginForm(){
