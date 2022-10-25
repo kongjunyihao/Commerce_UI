ec0f7bc894881c18814e1f0d1458074894931834
kongjunyihao <xutianxiang47@gmail.com>
2022-10-25 11:58:28 -0700

    Style

--- src/LoginForm.js
+++ src/LoginForm.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "./loginformStyle.css";
+import "./CSS/loginformStyle.css";
 
 function LoginForm(){
   const [username, setUsername] = useState("");
