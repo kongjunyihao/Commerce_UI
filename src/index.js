0e23af03f83717095fbe09e1ea996ff9ae1ffb96
kongjunyihao <xutianxiang47@gmail.com>
2022-10-27 11:05:53 -0700

    Sign In/Up

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import LoginForm from './LoginForm';
+import SignIn from './SignIn';
+import SignUp from './SignUp';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <LoginForm />
+    {/* <SignIn /> */}
+    <SignUp />
   </React.StrictMode>
 );
 
