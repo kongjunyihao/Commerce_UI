c381c9e0a8cee947562abe8da1e6b83988c456ec
kongjunyihao <74689780+kongjunyihao@users.noreply.github.com>
2022-11-11 09:49:29 -0800

    Add files via upload

---
+++ src/Components/NavBar.js
@@ -0,0 +1,109 @@
+import React from 'react';
+import { useState } from 'react';
+import { Linl as RouteLink } from 'react-router-dom';
+import { styled, useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import CssBaseline from '@mui/material/CssBaseline';
+import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+import { Tabs, Button, Tab } from '@mui/material';
+import ShoppingCartCheckoutIcon from '@mui/icons-material/ShoppingCartCheckout';
+
+const drawerWidth = 300;
+
+const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })(
+  ({ theme, open }) => ({
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: `-${drawerWidth}px`,
+    ...(open && {
+      transition: theme.transitions.create('margin', {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+      marginLeft: 0,
+    }),
+  }),
+);
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== 'open',
+})(({ theme, open }) => ({
+  transition: theme.transitions.create(['margin', 'width'], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: `${drawerWidth}px`,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+
+export default function CommerceNavBar(){
+    const theme = useTheme();
+    const [value, setValue] = useState(0);
+    const [open, setOpen] = useState(false);
+
+    const handleDrawerOpen = () => {
+        setOpen(true);
+    };
+
+    const handleDrawerClose = () => {
+        setOpen(false);
+    };
+
+    return (
+        <Box>
+            <CssBaseline />
+            <AppBar
+             sx={{ position: "static", background: "#fff" }}>
+                <Toolbar>
+                    <Tabs
+                    textColor="inherit"
+                    value={value}
+                    onChange={(e, value)=>setValue(value)}
+                    indicatorColor='secondary'
+                    >
+                      <Tab label="Home" sx={{color: 'black'}} />
+                      <Tab label="Best Sellers" sx={{color: 'black'}} />
+                      <Tab label="Store Basics" sx={{color: 'black'}} />
+                      <Tab label="Customer Services" sx={{color: 'black'}} />
+                      <Tab label="Todqay's Deals" sx={{color: 'black'}} />
+                      <Tab label="New Releases" sx={{color: 'black'}} />
+                      <Tab label="Books" sx={{color: 'black'}} />
+                      <Tab label="Music" sx={{color: 'black'}} />
+                      <Tab label="Pharmacy" sx={{color: 'black'}} />
+                      <Tab label="Household" sx={{color: 'black'}} />
+                      <Tab label="Fashion" sx={{color: 'black'}} />
+                    </Tabs>
+                </Toolbar>
+            </AppBar>
+            <Main open={open}>
+                
+            </Main>
+        </Box>
+    );
+}
