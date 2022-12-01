77a18bf92d634322ff6bab596e5c0bf94898ae82
Andonidas <152391221@qq.com>
2022-12-01 10:13:05 -0800

    Fixed rating style

--- src/Pages/Rating.js
+++ src/Pages/Rating.js
@@ -1,29 +1,11 @@
-import { v4 as uuidv4 } from "uuid";
+import Rating from '@mui/material/Rating';
+import Stack from '@mui/material/Stack';
 
-const Rating = ({ value, text, color }) => {
-  return (
-    <div className='rating'>
-      {[1, 2, 3, 4, 5].map((rate) => (
-        <span key={uuidv4()}>
-          <i
-            style={{ color }}
-            className={
-              value + 1 === rate + 0.5
-                ? "fas fa-star-half-alt"
-                : value >= rate
-                ? "fas fa-star"
-                : "far fa-star"
-            }
-          ></i>
-        </span>
-      ))}
+export default function ProductRating({value, text}){
+  return(
+    <Stack spacing={1}>
+      <Rating name="half-rating-read" value={value} precision={0.5} readOnly/>
       <span>{text && text}</span>
-    </div>
+    </Stack>
   );
-};
-
-Rating.defaultProps = {
-  color: "#FFA41C",
-};
-
-export default Rating;
+}
