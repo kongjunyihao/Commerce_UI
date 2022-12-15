f0747e5d5a91c2b81c1da849878dafcdc02eda34
Andonidas <152391221@qq.com>
2022-12-15 13:29:18 -0800

    Fixed relevant items

--- src/Components/Recommendation.js
+++ src/Components/Recommendation.js
@@ -4,7 +4,7 @@ import { Route, Routes, Link, useNavigate } from "react-router-dom";
 
 import "./SearchComponent/BestClothStyle.css";
 
-export default function({categoryVal}){
+export default function({categoryVal,productID}){
     const [cloth,setCloth] = useState([]);
     const Globalstate = useContext(CommerceContext);
     const dispatch = Globalstate.dispatch;
@@ -35,9 +35,13 @@ export default function({categoryVal}){
             <div className="recommend-comp">
             {cloth.map((item, index) => {
                 item.quantity = 1;
-                return (
+                if(item.id === parseInt(productID)){
+                    return;
+                }
+                else{
+                    return (
                         <div className="info" key={index}>
-                            <Link to={`${item.id}`} state={item}>
+                            <Link to={`/${item.id}`} state={item}>
                             <img id='recommend-img' src={
                                 item.image[0] === 'h'? item.image:require("../Asset/"+item.image) //apply online data / mock data
                                 } alt={item.title} />
@@ -46,7 +50,8 @@ export default function({categoryVal}){
                             <h3>$ {item.price}</h3>
                         </div>
                     );
-                })}
+                }
+            })}
             </div>
         </>
     );
