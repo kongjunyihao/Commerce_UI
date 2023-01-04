c379cdf2529f8d32bae20de26e3e15a2e3ac5347
kongjunyihao <xutianxiang47@gmail.com>
2023-01-04 12:56:01 -0800

    new

--- Server/router-product.js
+++ Server/router-product.js
@@ -5,20 +5,20 @@ const bcrypt = require('bcrypt')
 const path = require('path')
 const multer = require('multer')
 
-const productInfoTemplateCopy = require('./models/ProductModels')
+const productInfoTemplateCopy = require('./modules/ProductModels')
 
 
-//Storage
+//set up storage position
 const Storage = multer.diskStorage({
     destination: 'uploads',
     filename: (req, file, cb) =>{
         // cb(null, file.originalname)
         let ext = path.extname(file.originalname)
         cb(null, Date.now() + ext)
-    },
+    }
 })
 
-//Upload image
+//Upload image content
 const upload = multer({
     storage: Storage
 }).single('productImage')
@@ -37,7 +37,7 @@ router.post('/upload', (req, res)=>{
                 //     data: req.file.filename,
                 //     contentType: "image/png",
                 // },
-                productImge: req.file.path,
+                productImage: req.file.path,
                 price: req.body.price,
                 category: req.body.category,
                 rating: req.body.rating,
@@ -57,17 +57,99 @@ router.post('/upload', (req, res)=>{
     })
 })
 
-//show product detail
-router.get('/products/:id', async (req, res)=>{
-    const id = req.params.id
-    await productInfoTemplateCopy.findOne({id,})
-    .select(['-_id'])
-    .then((product)=>{
-        res.json(product)
-    })
+//show product list
+router.get('/products', async (req, res, next)=>{
+    try{
+        const result = await productInfoTemplateCopy.find({}, {__v: 0})
+        res.send(result)
+    }catch(error){
+        console.log(error.message)
+    }
 })
 
-//search
+//show product detail
+router.get('/products/:id', async (req, res)=>{
+    try{
+        const id = req.params.id
+        const result = await productInfoTemplateCopy.findOne({productID: id})
+        res.send(result)
+    }catch(error){
+        console.log(error.message)
+    }
+})
+
+//get product category
+router.get("/categories", async (req, res)=>{
+    try{
+        const result = await productInfoTemplateCopy.distinct('category')
+        res.send(result)
+    }catch(error){
+        console.log(error.message)
+    }
+})
+
+//get product in category
+router.get('/category/:category', async (req, res)=>{
+    const category = req.params.category
+    try{
+        const result = await productInfoTemplateCopy.find({category,})
+        res.send(result)
+    }catch(error){
+        console.log(error.message)
+    }
+})
+
+//update product
+let updateProduct = (req, res)=>{
+    upload(req, res, (err)=>{
+        if(err){
+            console.log(err)
+        }else{
+            const productInfo = new productInfoTemplateCopy({
+                productID: req.params.id,
+                productName: req.body.productName,
+                productType: req.body.productType,
+                productImage: req.file.path,
+                price: req.body.price,
+                category: req.body.category,
+                rating: req.body.rating,
+                view: req.body.view,
+                description: req.body.description
+            })
+            productInfo.save()
+            .then(data=>{
+                res.status(200).json(data)
+                console.log('successfully upload')
+            })
+            .catch(error=>{
+                res.json(error)
+                console.log(err)
+            })
+        }
+    })
+}
+
+router.put('/:id', updateProduct)
+router.patch('/:id', updateProduct)
+
+//delete product
+router.delete('/:id', async (req, res)=>{
+    if(req.params.id === null){
+        res.json({
+			status: 'error',
+			message: 'cart id should be provided',
+		})
+    }else{
+        try{
+            const result = await productInfoTemplateCopy.findOne({productID: req.params.id})
+            res.send(result)
+        }catch(error){
+            console.log(error.message)
+        }
+    }
+})
+
+//search bar
 router.post('/search', async (req, res)=>{
     let payload = req.body.payload.trim()
     let search = await productInfoTemplateCopy.find({name: {$regex: new RegExp('^'+payload+'.*','i')}}).exec()
@@ -76,11 +158,5 @@ router.post('/search', async (req, res)=>{
     res.send({payload: search})
 })
 
-//list data:
-router.get('/show', (req, res)=>{
-    const data = signUpTemplateCopy.find((err, listdata)=>{
-        res.json(listdata)
-    })
-})
 
 module.exports = router
