93c4c782fc18a5a7d6b071f1cc72f55daa6c8a3a
kongjunyihao <xutianxiang47@gmail.com>
2023-02-10 11:43:56 -0800

    my list page further design

--- src/Components/SearchComponent/MyList.js
+++ src/Components/SearchComponent/MyList.js
@@ -10,13 +10,45 @@ import Checkbox from '@mui/material/Checkbox';
 import Typography from '@mui/material/Typography';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 
-export default function MyList({productID}) {
-    const [items,setItems] = useState([])
+export default function MyList({ productID }) {
+    const [items, setItems] = useState([])
     const [itemDetails, setDetails] = useState([])
 
-  return (
-    <>
-        <Toolbar>
+    useEffect(() => {
+        fetch("http://localhost:4000/app/mylist", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                email: window.localStorage.getItem("email")
+            })
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data) setItems(data.products)
+            })
+    }, []);
+
+    useEffect(() => {
+        let details = [];
+        items.forEach((i) => {
+            fetch("http://localhost:4000/app/products/" + i.productID)
+                .then(res => res.json())
+                .then(data => {
+                    details.push(data);
+                    setDetails([...details]);
+                })
+        })
+    }, [items])
+
+    const handleAddToCart = () => { }
+
+    const handleRemove = () => { }
+
+    return (
+        <>
+            <Toolbar>
                 <Typography
                     variant="h4"
                     component="div"
@@ -27,12 +59,25 @@ export default function MyList({productID}) {
             </Toolbar>
             <div className='list-container'>
                 <div className='list-info'>
-                    {items.length === 0? (
-                        <h1 style={{marginTop: "15%", marginBottom: "15%", marginLeft:"40px"}}>Your cart is empty. Add something you like here!</h1>
+                    {items.length === 0 ? (
+                        <h1 style={{ marginTop: "15%", marginBottom: "15%", marginLeft: "40px" }}>Your cart is empty. Add something you like here!</h1>
                     ) : null}
-                    {items.map((item, index) => {})}
+                    {items.map((item, index) => {
+                        return (
+                            <div className='list-item' key={index}>
+                                <img src={
+                                    detail.productImage[0] === 'h' ? detail.productImage : require("../../../uploads/" + detail.productImage.slice(8, detail.productImage.length)) //apply online data / mock data
+                                } alt=""></img>
+                                <p>{detail.productName}</p>
+                                <div className='list-operation'>
+                                    <button onClick={handleAddToCart}>Add to Cart</button>
+                                    <button onClick={handleRemove}>Remove From List</button>
+                                </div>
+                            </div>
+                        );
+                    })}
                 </div>
             </div>
-    </>
-  );
+        </>
+    );
 }
