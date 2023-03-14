22a5ecaedd0759384802b626025c47290b66fcdf
kongjunyihao <xutianxiang47@gmail.com>
2023-03-13 17:16:55 -0700

    delete comment

--- src/SellerComponent/SellerCenterNavBar.js
+++ src/SellerComponent/SellerCenterNavBar.js
@@ -2,26 +2,10 @@ import React from 'react';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { styled, useTheme } from '@mui/material/styles';
-// import Box from '@mui/material/Box';
-// import Drawer from '@mui/material/Drawer';
 import CssBaseline from '@mui/material/CssBaseline';
 import MuiAppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
-// import List from '@mui/material/List';
-// import Typography from '@mui/material/Typography';
-// import Divider from '@mui/material/Divider';
-// import IconButton from '@mui/material/IconButton';
-// import MenuIcon from '@mui/icons-material/Menu';
-// import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-// import ChevronRightIcon from '@mui/icons-material/ChevronRight';
-// import ListItem from '@mui/material/ListItem';
-// import ListItemButton from '@mui/material/ListItemButton';
-// import ListItemIcon from '@mui/material/ListItemIcon';
-// import ListItemText from '@mui/material/ListItemText';
-// import InboxIcon from '@mui/icons-material/MoveToInbox';
-// import MailIcon from '@mui/icons-material/Mail';
 import { Tabs, Tab } from '@mui/material';
-// import ShoppingCartCheckoutIcon from '@mui/icons-material/ShoppingCartCheckout';
 const drawerWidth = 300;
 
 const AppBar = styled(MuiAppBar, {
@@ -43,17 +27,8 @@ const AppBar = styled(MuiAppBar, {
 
 
 export default function SellerCenterNavBar(){
-    // const theme = useTheme();
     const [value, setValue] = useState(0);
-    // const [open, setOpen] = useState(false);
     const navigate = useNavigate();
-    // const handleDrawerOpen = () => {
-    //     setOpen(true);
-    // };
-
-    // const handleDrawerClose = () => {
-    //     setOpen(false);
-    // };
 
     return (
         <>
