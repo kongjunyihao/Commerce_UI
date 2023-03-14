385715599e6d476394b19ba53ed8b47599a28845
kongjunyihao <xutianxiang47@gmail.com>
2023-03-13 17:05:22 -0700

    delete comment

--- src/Components/SearchComponent/addressPage.js
+++ src/Components/SearchComponent/addressPage.js
@@ -1,15 +1,12 @@
 import React, { useState} from 'react';
 import { useNavigate } from "react-router-dom";
 
-// import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
-// import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
-// import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
