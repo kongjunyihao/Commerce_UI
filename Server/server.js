7bddd853a13193ebcacc5daaa171ab1a938966cf
kongjunyihao <xutianxiang47@gmail.com>
2023-03-15 01:31:27 -0700

    modify srver

--- Server/server.js
+++ Server/server.js
@@ -43,7 +43,7 @@ connect()
 app.use(cors())
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "*");
     next();
 });
 app.use(express.json())
