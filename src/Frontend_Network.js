d8cd520887e08656507b0859e06c7edf2d5562a0
kongjunyihao <xutianxiang47@gmail.com>
2023-03-17 10:36:20 -0700

    mock

--- src/Frontend_Network.js
+++ src/Frontend_Network.js
@@ -1,1 +1,23 @@
 export const Host = 'http://107.20.70.11:80';
+
+const first = () => (new Promise((resolve, reject) => {
+    console.log(3);
+    let p = new Promise((resolve, reject) => {
+    console.log(7);
+    setTimeout(() => {
+    console.log(5);
+    resolve(6);
+    }, 0);
+    resolve(1);
+    });
+    resolve(2);
+    p.then((arg) => {
+    console.log(arg);
+    });
+    
+    }));
+    first().then((arg) => {
+    console.log(arg);
+    })
+    
+    console.log(4);
